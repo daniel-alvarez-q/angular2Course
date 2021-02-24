c5e0f51fd888c3da47995a1c2b41ebc78534cb6a
Daniel Alvarez <danielalvarez@Daniels-Air.lan>
2021-02-25 00:04:52 +0100

    Set basic behavior for ingredients list (add a new ingredient to the list)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static:true}) nameInputRef:ElementRef;
+  @ViewChild('amountInput', {static:true}) amountInputRef:ElementRef;
+
+  @Output () ingredientAdded = new EventEmitter <Ingredient> (); //instead of passing an object I preferred to pass the Ingredient model per se
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient(){
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    this.ingredientAdded.emit(new Ingredient (name, amount));
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
+  onIngredientAdded(i:Ingredient){
+    this.ingredients.push(i);
+  }
+
+} 
